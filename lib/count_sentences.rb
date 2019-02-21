bce69c2bdcbd9053f229b44e14e9bd1862829823
Taylor Swinton <taylorswinton1@gmail.com>
2019-02-21 20:24:15 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,30 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    #count = 0
+    ourSentences = self.split
+    ourSentences.scan(/[^\.!?]+[\.!?]/).map(&:strip).count
+=begin
+    ourSentences.each do |sentence|
+      if sentence.empty? == false
+        count += 1
+      else sentence.empty? == true
+        sentence.delete!
+      end
+    end
+    return count
+=end
   end
 end
